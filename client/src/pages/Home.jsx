39b3263cd1c71659799e04af4c942f759bfe75ae
HIMA CHARAN GANGULA <himacharan128@gmail.com>
2023-06-14 21:04:14 +0530

    routing_done

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react'
+import { Loader, Card, FormField} from '../components'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
